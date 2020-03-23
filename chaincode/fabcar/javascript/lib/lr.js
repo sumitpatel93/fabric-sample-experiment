b3dfe19d9ee7eac5d88a39b33c95fe1aa1752ac2
sumit patel <sumit.patel93@gmail.com>
2020-03-23 05:40:24 +0530

    Update lr.js

--- chaincode/fabcar/javascript/lib/lr.js
+++ chaincode/fabcar/javascript/lib/lr.js
@@ -10,216 +10,310 @@ const ClientIdentity = require('fabric-shim').ClientIdentity;
 class LandRegistration extends Contract {
 
 
-    async init(ctx) {
+    async init(stub) {
         return shim.success()
     }
 
-    async invoke() {
 
+
+    async invoke(stub, args) {
+        let ret = stub.getFunctionAndParameters();
+        console.info(ret);
+
+        let method = this[ret.fcn];
+
+        if (!method) {
+            console.error('no function of name:' + ret.fcn + ' found');
+            throw new Error('Received unknown function ' + ret.fcn + ' invocation');
+        }
+        try {
+            let payload = await method(stub, ret.params);
+            return shim.success(payload);
+        } catch (err) {
+            console.info(err);
+            return shim.error(err);
+        }
     }
 
-    async createLandRecord(ctx, args ) {
-        if ( args.length != 4) {
+
+
+
+    async createLandRecord(ctx, args) {
+        if (args.length != 4) {
             throw new Error('Incorrect number of arguments,Expected number of arguments is 4');
         }
-        let landId = args[0];
-        let ownerId = args[1];
-        let  ownerName = args[2];
-        let  landStatus = 'NEW';
-        let saleDeed = args[3]
 
-        const landDetail = {
-            landId: landId,
-            ownerId: ownerId,
-            ownerName: ownerName,
-            landStatus: landStatus,
-            saleDeed: saleDeed
+        let cid = new ClientIdentity(ctx.stub);
+        if (cid.assertAttributeValue('userType', 'BOR')) {
+
+            let _landId = args[0];
+            let _ownerId = args[1];
+            let _ownerName = args[2];
+            let _landStatus = 'NEW';
+            let _saleDeedId = args[3];
+
+
+            let land = await stub.getState(_landId);
+            if (land.toString()) {
+                throw new Error('This marble already exists' + _landId);
+            }
+
+            let landDetail = {};
+            landDetail.landId = _landId;
+            landDetail.ownerId = _ownerId;
+            landDetail.ownerName = _ownerName;
+            landDetail.landStatus = _landStatus;
+            landDetail.saleDeed = _saleDeedId;
+
+
+            await ctx.stub.putState(_landId, Buffer.from(JSON.stringify(landDetail)));
+            console.info(JSON.stringify(landDetail));
+            console.log("Land Id created successfully");
+            return shim.success();
+
+        } else {
+            throw new Error('Not a valid user');
         }
-        let land = await stub.getState(landId);
-        if ( land.toString()){
-            throw new Error('This marble already exists' + landId);
-        }
-
-        let landDetail = {};
-        landDetail.landId = landId;
-        landDetail.ownerId = ownerId;
-        landDetail.ownerName = ownerName;
-        landDetail.landStatus = landStatus;
-        landDetail.saleDeed = saleDeed;
-
-
-        await ctx.stub.putState(landId, Buffer.from(JSON.stringify(landDetail)));
-        console.info(JSON.stringify(landDetail));
-        console.log("Land Id created successfully");
-        return shim.success();
     }
 
 
-    //INCOMPLETE
+
     async createSaleDeed(ctx, args) {
-        if ( args.length != 6) {
+        //check for the correct number of arguments, else throw err
+        if (args.length != 6) {
             throw new Error('Incorrenct number of arguments,Expected number of arguments is 6');
         }
-        let docType = args[0];
-        let saleDeedId = args[1];
-        let landId = args[2];
-        let sellerId = args[3];
-        let buyerId = args[4];
-        let buyerName = args[5]
 
 
-        let saleDeed = await stub.getState(saleDeedId);
-        if (saleDeedId.toString()){
-            throw new Error('This saledeed already exists ' + saleDeedId);
-        }
+        let cid = new ClientIdentity(ctx.stub);
+        //check for the correct role
+        if (cid.assertAttributeValue('userType', 'DOSR')) {
+            let _docType = args[0];
+            let _saleDeedId = args[1];
+            let _landId = args[2];
+            let _sellerId = args[3];
+            let _buyerId = args[4];
+            let _buyerName = args[5]
 
+
+            //create sale deed object and assign values
             const saleDeed = {}
-            saleDeed.docType = docType,
-            saleDeed.saleDeedId = saleDeedId,
-            saleDeed.landId = landId,
-            saleDeed.sellerId = sellerId,
-            saleDeed.buyerId = buyerId,
-            saleDeed.buyerName = buyerName
-        
+            saleDeed.docType = _docType,
+                saleDeed.saleDeedId = _saleDeedId,
+                saleDeed.landId = _landId,
+                saleDeed.sellerId = _sellerId,
+                saleDeed.buyerId = _buyerId,
+                saleDeed.buyerName = _buyerName
 
-        let saleDeedAsBytes = await stub.getState(saleDeedId);
-        if(!saleDeedAsBytes.toString()){
-            console.log("sale deed does not exist")
+            //check if the saledeedid already exists 
+            let saleDeedAsBytes = await stub.getState(_saleDeedId);
+            if (!saleDeedAsBytes.toString()) {
+                console.log("sale deed does not exist")
+            } else if (saleDeedAsBytes !== null) {
+                let result = JSON.parse(saleDeedAsBytes);
+                console.log('sale deed already exsts' + result);
+                return JSON.stringify(result);
+            } else {
+                //check for landId status
+                let landAsBytes = await stub.getState(_landId)
+                if (!landAsBytes.toString()) {
+                    let jsonresp = {};
+                    jsonresp.Error = 'Land Id doesnt exist';
+                    throw new Error(JSON.stringify(jsonresp));
+                } else if (landAsBytes == null) {
+                    let result = JSON.parse(landAsBytes)
+                    console.log('Land results are empty');
+                } else {
+                    let landDetail = {};
+                    let landRecordStatus = landDetail.landStatus;
+
+                    if (landRecordStatus == "NEW" || landRecordStatus == "OPEN" || landRecordStatus == "MUTATION_DONE") {
+                        landDetail.landStatus = "REGISTRATION_DONE";
+                        landDetail.saleDeedId = _saleDeedId
+                        await ctx.stub.putState(saleDeedId, Buffer.from(JSON.stringify(saleDeed)));
+                        console.log('sale deed successfully created');
+                        console.info(JSON.stringify(saleDeed));
+                        return shim.success();
+                    }
+                }
+            }
         } else {
-           let result = JSON.parse(saleDeedAsBytes);
-           console.log(result);
-           return JSON.stringify(result);
+            throw new Error('Not a valid user');
         }
-        
-        let landDetail = {};
-        let landRecordStatus = landDetail.landStatus;
-
-        if (landRecordStatus == "NEW" || landRecordStatus == "OPEN" || landRecordStatus == "MUTATION_DONE") {
-            await ctx.stub.putState(saleDeedId, Buffer.from(JSON.stringify(saleDeed)));
-            console.log('sale deed successfully created');
-            console.info(JSON.stringify(saleDeed));
-            return shim.success();
-        }      
     }
 
 
-    async getSaleDeed(ctx, args) {     
-
-        if ( args.length != 1) {
+    async getSaleDeed(ctx, args) {
+        if (args.length != 1) {
             throw new Error('Incorrenct number of arguments,Expected number of arguments is 1');
         }
-        let saleDeedId = args[0];
-        console.log('query made for ' + saleDeedId);
 
-        let returnAsBytes = await ctx.stub.getState(saleDeedId);
-        console.log(returnAsBytes);
-        if (!returnAsBytes) {
-            return new Error(`${saleDeedId} does not exist`);
+        let cid = new ClientIdentity(ctx.stub);
+        if (cid.assertAttributeValue('userType', 'DOSR')) {
+            let saleDeedId = args[0];
+            console.log('query made for ' + saleDeedId);
+
+            let returnAsBytes = await ctx.stub.getState(saleDeedId);
+            console.log(returnAsBytes);
+            if (!returnAsBytes) {
+                return new Error(`${saleDeedId} does not exist`);
+            } else {
+                let result = JSON.parse(returnAsBytes);
+                console.log(result);
+                return JSON.stringify(result);
+            }
+
         } else {
-            let result = JSON.parse(returnAsBytes);
-            console.log(result);
-            return JSON.stringify(result);
+            throw new Error('Not a valid user');
         }
     }
 
 
     async getLandRecord(ctx, args) {
-        if ( args.length != 1) {
+        if (args.length != 1) {
             throw new Error('Incorrenct number of arguments,Expected number of arguments is 1');
         }
-        let landId = args[0];
-        console.log('query made for ' + landId);
+        let cid = new ClientIdentity(ctx.stub);
+        if (cid.assertAttributeValue('userType', 'BOR')) {
+            let landId = args[0];
+            console.log('query made for ' + landId);
 
-        let landDetailAsBytes = await ctx.stub.getState(landId);
-        console.log(landDetailAsBytes);
+            let landDetailAsBytes = await ctx.stub.getState(landId);
+            console.log(landDetailAsBytes);
 
-        if (!landDetailAsBytes ) {
-            return new Error(`${landId} does not exist`);
+            if (!landDetailAsBytes) {
+                return new Error(`${landId} does not exist`);
+            } else {
+                let landIdResult = JSON.parse(landDetailAsBytes);
+                console.log(landIdResult);
+                return JSON.stringify(landIdResult);
+            }
         } else {
-            let landIdResult = JSON.parse(landDetailAsBytes);
-            console.log(landIdResult);
-            return JSON.stringify(landIdResult);
+            throw new Error('Not a valid user');
         }
+
+
+
+
     }
 
     async getBuyerFromSaleDeed(ctx, args) {
-        if ( args.length != 1) {
+
+        if (args.length != 1) {
             throw new Error('Incorrenct number of arguments,Expected number of arguments is 1');
         }
-        let saleDeedId = args[0];
-        console.log('query made for ' + saleDeedId)
+        let cid = new ClientIdentity(ctx.stub);
+        if (cid.assertAttributeValue('userType', 'BOR')) {
+            let saleDeedId = args[0];
+            console.log('query made for ' + saleDeedId)
+            let saleDeedAsBytes = await ctx.stub.getState(saleDeedId)
+            console.log(saleDeedAsBytes);
 
-
-        let saleDeedAsBytes = await ctx.stub.getState(saleDeedId)
-        console.log(saleDeedAsBytes);
-
-        if (!saleDeedAsBytes) {
-            return new Error(`${saleDeedId} does not exist`);
+            if (!saleDeedAsBytes) {
+                return new Error(`${saleDeedId} does not exist`);
+            } else {
+                let saleDeedResult = JSON.parse(saleDeed);
+                console.log(saleDeedResult.buyerName);
+                return JSON.stringify(saleDeedResult.buyerName);
+            }
         } else {
-            let saleDeedResult = JSON.parse(saleDeed);
-            console.log(saleDeedResult);
-            return JSON.stringify(saleDeedResult);
+            throw new Error('Not a valid user');
         }
-
-
     }
+
+
+
 
     async getLandRecordStatus(ctx, args) {
-        if ( args.length != 1) {
+        if (args.length != 1) {
             throw new Error('Incorrenct number of arguments,Expected number of arguments is 1');
         }
-        let landId = args[0];
-        console.log('query made for the ' + landId);
-        
-        let landDetailBytes = await ctx.stub.getState(landId);
-        console.log(landDetailBytes);
-        if (!landDetailBytes) {
-            return new Error(`${landId} doesnot exist`);
+        let cid = new ClientIdentity(ctx.stub);
+        if (cid.assertAttributeValue('userType', 'BOR')) {
+
+            let landId = args[0];
+            console.log('query made for the ' + landId);
+
+            let landDetailBytes = await ctx.stub.getState(landId);
+            console.log(landDetailBytes);
+            if (!landDetailBytes) {
+                return new Error(`${landId} doesnot exist`);
+            } else {
+                let landIdResult = JSON.parse(landDetail);
+                console.log(landIdResult.landStatus);
+                return JSON.stringify(landDetail.landStatus);
+
+            }
         } else {
-            let landIdResult = JSON.parse(landDetail);
-            console.log(landIdResult.landStatus);
-            return JSON.stringify(landDetail.landStatus);
-            
+            throw new Error('Not a valid user');
         }
     }
-    
+
+
+
+
     //INCOMPLETE
-    // async mutateLandRecord(ctx, args) {
-    //     if ( args.length != 2) {
-    //         throw new Error('Incorrect number of arguments , expected number of arguments is 2');
-    //     }
+    async mutateLandRecord(ctx, args) {
+        if (args.length != 3) {
+            throw new Error('Incorrect number of arguments , expected number of arguments is 2');
+        }
+        let cid = new ClientIdentity(ctx.stub);
+        if (cid.assertAttributeValue('userType', 'BOR')) {
+            let landId = args[0];
+            let newOwnerName = args[1];
+            let newOwnerId = args[2];
+            console.info('- start mutation ', landId, newOwnerName,newOwnerId);
 
-    //     let landId = args[0];
-    //     let newOwnerName = args[1];
 
+            let landresults = await ctx.stub.getState(landId);
+            if (!landresults) {
+                return new Error(`${landId} Failed to get land record`);
+            } else if (landresults === null) {
+                return shim.Error('Land record does not exist')
+            } else {
+                let landToTransfer = {}
+                try {
+                    let landRecordStatus = landresults.landRecordStatus;    
+                    if (landRecordStatus == "REGISTRATION_DONE")
+                    {
+                        landToTransfer.ownerId = newOwnerId;
+                        landToTransfer.ownerName = newOwnerName;
+                        landToTransfer.landStatus = "MUTATION_DONE"
+                    }
+                    await ctx.stub.putState(landId, Buffer.from(JSON.stringify(landToTransfer)));
+                    console.info('Success, mutation done');
+                } catch (err) {
+                    let jsonresp = {};
+                    jsonresp.error = "Failed to decode json of";
+                    throw new Error(jsonresp);
+                }            
+            }           
 
-    //     let landresults = await ctx.stub.getState(landId);
-    //     console.log(landresults);
-
-    //     if(!landresults ){
-    //         return new Error (`${landId} Failed to get land record`);
-    //     } else if ( landresults === null ){
-    //         return  shim.Error('Land record does not exist')
-
-    //     }
-
-    //     landToTransfer = {};
-    //     landRecordStatus = landToTransfer.landRecordStatus
-    //     saleDeedId = landToTransfer.saleDeedId
-
-    // }
-
-    async getLandRecordLifeCycle(ctx, args) {
-        if (args.length < 1 ){
-            throw new Error('Incorrect number of arguments.Expecting 1');
+        } else {
+            throw new Error('Not a valid user');
         }
 
-        let landId = args[0];
-        console.log('gettingland record life cycle for',landId);
 
-        let resultsIterator = await ctx.stub.getHistoryForKey(landId);
-        console.log(resultsIterator);
-        return JSON.stringify(getHistoryForKey);
+
+
+
+    }
+
+    async getLandRecordLifeCycle(ctx, args) {
+        if (args.length < 1) {
+            throw new Error('Incorrect number of arguments.Expecting 1');
+        }
+        let cid = new ClientIdentity(ctx.stub);
+        if (cid.assertAttributeValue('userType', 'DOSR')) {
+            let landId = args[0];
+            console.log('gettingland record life cycle for', landId);
+
+            let resultsIterator = await ctx.stub.getHistoryForKey(landId);
+            console.log(resultsIterator);
+            return JSON.stringify(getHistoryForKey);
+        } else {
+            throw new Error('Not a valid user');
+        }
     }
 }
 shim.start(new LandRegistration())
